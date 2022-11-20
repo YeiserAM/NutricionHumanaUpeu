c7fef7529628085675ef671e42ae8f0f7a58c794
yeiser <yeiseravila@upeu.edu.pe>
2022-11-20 02:40:32 -0500

    ultimos cambios

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  private URL = 'https://bd-nutricion.herokuapp.com';
+
+
+  constructor(private http: HttpClient) { }
+  
+  singin(user:any){
+    return this.http.post(`${this.URL}/api/users`,user);
+  }
 }
