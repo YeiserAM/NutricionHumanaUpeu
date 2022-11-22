d67d3942fdc6acb35aeb801509dcd0d7698ad4b3
Productos-Union <manager@union.pe>
2022-11-21 23:56:20 -0500

    login terminate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -30,6 +31,7 @@ import { CargadocsComponent } from './components/cargadocs/cargadocs.component';
     FormsModule,
     HttpClientModule,
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     ToastrModule.forRoot()
   ],
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  url: 'https://nutricion-backend.herokuapp.com'
+};
